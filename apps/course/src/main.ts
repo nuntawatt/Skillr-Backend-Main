d77befbeda92561be26a072f9514a299019ddb76
Nuntawat Saehuam <nanthawat.s@kkumail.com>
2026-01-12 12:40:05 +0700

    fix: listen port

--- apps/course/src/main.ts
+++ apps/course/src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
 
 
   const port = Number(process.env.PORT ?? 3002);
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`Application is running on http://localhost:${port}/api`);
   logger.log(`Swagger docs available at http://localhost:${port}/api/docs`);
