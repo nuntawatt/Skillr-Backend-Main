28b41189dbd1e5861446f7e47bfeca27dbdf3e62
Nuntawat Saehuam <nanthawat.s@kkumail.com>
2026-01-27 17:14:54 +0700

    refactor: update LessonType enum values to use consistent lowercase casing

--- apps/course/src/lessons/entities/lesson.entity.ts
+++ apps/course/src/lessons/entities/lesson.entity.ts
@@ -3,9 +3,9 @@ import { Chapter } from '../../chapters/entities/chapter.entity';
 import { Article } from '../../articles/entities/article.entity';
 
 export enum LessonType {
-  ARTICLE = 'Article',
-  VIDEO = 'Video',
-  QUIZ = 'Quiz',
+  ARTICLE = 'article',
+  VIDEO = 'video',
+  QUIZ = 'quiz',
 }
 
 @Entity('lessons')
