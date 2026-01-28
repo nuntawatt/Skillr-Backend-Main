3e15598f2f26ee1e8941febde6fbe7fcf2fa2eec
Phollphat Wangsawat <honasas1101@gmail.com>
2026-01-28 16:13:55 +0700

    fix: update database configuration to disable synchronization
    - Changed the synchronize option from true to false in the database configuration to prevent automatic schema synchronization, aligning with the use of migrations.

--- libs/config/src/database.config.ts
+++ libs/config/src/database.config.ts
@@ -10,7 +10,7 @@ export const getDatabaseConfig = (
     type: 'postgres',
     url: databaseUrl,
     autoLoadEntities: true, // true or false
-    synchronize: true, // Disabling synchronize since we are using migrations
+    synchronize: false, // Disabling synchronize since we are using migrations
     logging: false, // Set to true if you need SQL query log
   };
 };
