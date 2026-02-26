fcce897078168dd13a8832611a02dc6a66b87e04
Nuntawat Saehuam <nanthawat.s@kkumail.com>
2026-02-26 21:06:18 +0700

    Update Action

--- apps/course/src/courses/courses.service.ts
+++ apps/course/src/courses/courses.service.ts
@@ -190,7 +190,7 @@ export class CoursesService {
             return {
               lesson_id: lesson.lesson_id,
               lesson_title: lesson.lesson_title,
-              lesson_type: lesson.lesson_type,
+              lesson_type: lesson.lesson_type, 
               lesson_description: lesson.lesson_description ?? undefined,
               ref_id: lesson.ref_id,
               orderIndex: lesson.orderIndex,
